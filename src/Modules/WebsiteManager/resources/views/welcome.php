1455bc5fa4017a5726aa94897adf8ee26943d493
Drox <thijmenwijers1@gmail.com>
2025-10-03 06:20:51 +0200

    initial

---
+++ src/Modules/WebsiteManager/resources/views/welcome.php
@@ -0,0 +1,9 @@
+<div id="welcome-page">
+
+    <h1 class="text-center">Landingspage Builder</h1>
+
+    <div class="text-center intro-links">
+        <a href="<?= phpb_url('website_manager') ?>">Beginnen</a>
+    </div>
+
+</div>
